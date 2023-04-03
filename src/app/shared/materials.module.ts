d312b86f396243fc30f99fe5203e8c06741099f6
laye <abmangane12@gmail.com>
2023-04-03 15:06:52 -0800

    recommit

--- src/app/shared/materials.module.ts
+++ src/app/shared/materials.module.ts
@@ -3,13 +3,15 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
 	exports: [
 		MatToolbarModule,
 		MatFormFieldModule,
 		MatInputModule,
-		MatRadioModule
+		MatRadioModule,
+		MatCardModule
 	]
 })
 
