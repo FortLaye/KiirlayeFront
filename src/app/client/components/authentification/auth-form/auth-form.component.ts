98045f9fa8bcfe32c71461fd0253dc4b602271fc
laye <abmangane12@gmail.com>
2023-04-05 11:34:18 -0800

    code review for home page

--- src/app/client/components/authentification/auth-form/auth-form.component.ts
+++ src/app/client/components/authentification/auth-form/auth-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Observable, map, startWith, tap } from 'rxjs';
 import { AuthentificationService } from 'src/app/client/services/authentification.service';
 
 
@@ -13,14 +12,7 @@ import { AuthentificationService } from 'src/app/client/services/authentificatio
 export class AuthFormComponent implements OnInit{
 
   loginForm!: FormGroup;
-  preferenceLoginCtrl!: FormControl
-  emailCtrl!: FormControl
-  phoneCtrl!: FormControl
-  numIpmCtrl!: FormControl
-
-  showEmailField$!: Observable<boolean>
-  showPhoneField$!: Observable<boolean>
-  showNumIpmField$!: Observable<boolean>
+  loginCtrl!: FormControl
 
   msgError!: string
   isError: boolean = false
@@ -30,71 +22,12 @@ export class AuthFormComponent implements OnInit{
               private router: Router){}
 
   ngOnInit(): void {
-    this.initFormControls();
-    this.initLoginForm();
-    this.initFormObservable()
-    
+    this.initForm();
   }
 
-  initFormObservable() {
-
-    this.showEmailField$ = this.preferenceLoginCtrl.valueChanges.pipe(
-      startWith(this.preferenceLoginCtrl.value),
-      map(preference => preference === "email"),
-      tap(showEmailCtrl => this.setEmailCtrlValidators(showEmailCtrl))
-    )
-    this.showPhoneField$ = this.preferenceLoginCtrl.valueChanges.pipe(
-      startWith(this.preferenceLoginCtrl.value),
-      map(preference => preference === "phone"),
-      tap(showPhoneCtrl => this.setPhoneCtrlValidators(showPhoneCtrl))
-    )
-    this.showNumIpmField$ = this.preferenceLoginCtrl.valueChanges.pipe(
-      startWith(this.preferenceLoginCtrl.value),
-      map(preference => preference === "ipm"),
-      tap(showNumIpmCtrl => this.setNumIpmCtrlValidators(showNumIpmCtrl))
-    )
-  }
-
-  setEmailCtrlValidators(showEmailCtrl: boolean){
-    if (showEmailCtrl) {
-      this.emailCtrl.addValidators([Validators.required, Validators.email])
-    } else {
-      this.emailCtrl.clearValidators()
-    }
-    this.emailCtrl.updateValueAndValidity();
-  }
-
-  setPhoneCtrlValidators(showPhoneCtrl: boolean){
-    if (showPhoneCtrl) {
-      this.phoneCtrl.addValidators([Validators.required, Validators.pattern("^[0-9]*$"), Validators.minLength(9), Validators.maxLength(9)])
-    } else {
-      this.phoneCtrl.clearValidators();
-    }
-    this.phoneCtrl.updateValueAndValidity();
-  }
-
-  setNumIpmCtrlValidators(showNumIpmCtrl: boolean){
-    if (showNumIpmCtrl) {
-      this.numIpmCtrl.addValidators([Validators.required, Validators.minLength(9), Validators.maxLength(9)])
-    }else{
-      this.numIpmCtrl.clearValidators();
-    }
-    this.numIpmCtrl.updateValueAndValidity();
-  }
-
-  private initFormControls(){
-    this.preferenceLoginCtrl = this.fb.control('email');
-    this.emailCtrl = this.fb.control('')
-    this.phoneCtrl = this.fb.control('')
-    this.numIpmCtrl = this.fb.control('')
-  }
-
-  private initLoginForm(){
+  private initForm(){
     this.loginForm = this.fb.group({
-      preference: this.preferenceLoginCtrl,
-      email: this.emailCtrl,
-      phone: this.phoneCtrl,
-      numIpm: this.numIpmCtrl,
+      login: ['', Validators.required, Validators.minLength(9)],
       password: ['', Validators.required]
     })
   }
@@ -102,10 +35,8 @@ export class AuthFormComponent implements OnInit{
   getFormCtrlEorrors(ctrl: AbstractControl){
       if (ctrl.hasError('required')) {
         return 'Ce champ est obligatoire !'
-      } else if(ctrl.hasError('email')) {
-        return 'Entrez une adresse mail valide !'
-      }else if(ctrl.hasError('pattern')){
-        return 'Entrez un numéro de téléphone valide !'
+      } else if(ctrl.hasError('minLength')){
+        return 'Vous devez entrer minimum 9 caractères !'
       } else {
         return 'Ce champ contient des erreurs !'
       }
