393fdefec2bde01d62d8efa2d6eaf297b619ce01
laye <abmangane12@gmail.com>
2023-05-22 12:18:11 -0800

    add entreprise

--- src/app/client/components/menu-principal/menu-principal.component.ts
+++ src/app/client/components/menu-principal/menu-principal.component.ts
@@ -31,13 +31,6 @@ export class MenuPrincipalComponent implements OnInit{
         }
       )
 
-        $(function() {
-            $("li").click(function(e) {
-                e.preventDefault();
-                $("li").removeClass("active");
-                $(this).addClass("active");
-            });
-        });
     }
 
     logout(){
